bb4fbb26924a11c17fa52ceaddbea5ddbcab8c56
unknown <46693400+devmachiine@users.noreply.github.com>
2019-02-08 22:24:23 +0100

    Save functions to disk

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var cache = require('./cache-barrel')('some file')
+var cache = require('./cache-barrel')('./shelf')
 var ff = require('./draw.js')(cache)
 
 let addPath = 'https://gist.githubusercontent.com/devmachiine/4433df78dc698aebad5aa37be15475fa/raw/2f980ee176dfa76d03dda4bf1737c3fe6a727eae/add.js'
--- cache-barrel.js
+++ cache-barrel.js
@@ -1,9 +1,9 @@
-module.exports = function (cacheFile) {
+module.exports = function (cacheDir) {
 
+    const fs = require('fs')
     const fetch = require('./node-fetch')
     let print = require('./dev-printer')()
 
-    print('barrel file:' + cacheFile)
     // devnote, if 1 file is insufficient, create subdirectories and files on base64names
 
     // check memory cache for function and return it, else
@@ -31,6 +31,50 @@ module.exports = function (cacheFile) {
 
     //let disk cache..
 
+    // Init file cache
+    print('barrel dir:' + cacheDir)
+    if (!fs.existsSync(cacheDir)) {
+        fs.mkdirSync(cacheDir);
+    }
+
+    let fileExists = (file) => new Promise((resolve, reject) => {
+        print('go stat : ' + file)
+        fs.stat(file, function (err, _stat) {
+            if (err == null) {
+                print(`file ${file} exists, reading`)
+                resolve(true);
+            } else if (err.code === 'ENOENT') {
+                print(`file ${file} doesn't exist`)
+                resolve(false);
+            } else {
+                console.log('Some other error: ', err.code);
+                reject('file exist error: ' + err.code);
+            }
+        })
+    })
+
+    let convertKeyToFilePath = (key) => cacheDir + '/test.internal' // or .external for remote dependency
+    let disk_fetch = (file) => new Promise((resolve, reject) => {
+        
+        print(`disk read file = ${file}`)
+        fileExists(file).then((exists) => {
+            if (exists) {
+                fs.readFile(file, (err, data) => {
+                    if (err) reject(err)
+                    else resolve(data)
+                })
+            } else resolve(undefined)
+        })
+    })
+    let disk_add = (key, value) => new Promise((resolve, reject) => {
+        let keyPath = convertKeyToFilePath(key)
+        fs.writeFile(keyPath, value, function (err) {
+            if (err) {
+                reject(err)
+            } else resolve(value) // or dont resolve to something specific
+        });
+    })
+
     //let memory cache..
     let memCache = new Map()
 
@@ -39,17 +83,35 @@ module.exports = function (cacheFile) {
 
     // let toBase64Key..
 
-    return (dependency) => {
+    let lookup = (dependency) => {
 
-        let memVal = mem_fetch(dependency);
+        let keyPath = convertKeyToFilePath(dependency)
+
+        let memVal = mem_fetch(keyPath);
 
         if (!memVal) {
-            print(`didn't find it in the cache, pulling it from the web`)
-            let web_val = web_fetch(dependency)
-            mem_add(dependency, web_val)
-            memVal = web_val
-        } else print(`loaded cached value !`)
+            print(`didn't find it in memory, pulling it from the filesystem`)
 
-        return memVal
+            return disk_fetch(keyPath).then((diskVal) => {
+
+                print(` -- disk val -> ${diskVal}`)
+                if (diskVal) {
+                    print('read value from disk: ' + diskVal)
+                    // load to memory
+                    mem_add(keyPath, diskVal)
+                    memVal = diskVal
+                    return Promise.resolve(memVal)
+                } else {
+                    print(`didn't find it on the filesystem, pulling it from the web`)
+                    return web_fetch(dependency).then((web_val) => {
+                        mem_add(keyPath, web_val)
+                        return web_val
+                    }).then((val) => disk_add(keyPath, val))
+                }
+
+            }).catch(err => print('oh file.. ' + err))
+        } else return Promise.resolve(memVal)
     }
+
+    return lookup
 }
