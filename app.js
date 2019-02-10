0dbccf090a7860687a5e1f08d2ad7e4e8d1bd573
unknown <46693400+devmachiine@users.noreply.github.com>
2019-02-10 18:16:30 +0100

    Refactor to demonstrate disk vs cached mem call times

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ var ff = require('./draw.js')(cache)
 
 let addPath = 'https://gist.githubusercontent.com/devmachiine/4433df78dc698aebad5aa37be15475fa/raw/2f980ee176dfa76d03dda4bf1737c3fe6a727eae/add.js'
 
-let startTime = Date.now()
-
-let demo_f = f => {
-    console.log("f is a -> " + f)
-    console.log('f(3,8) = ' + f(3, 8));
+let demo_f = path => {
+    let startTime = Date.now()
+    return ff(path).then((f) => {
+        console.log("f is a -> " + f)
+        console.log('f(3,8) = ' + f(3, 8));
+        let endTime = Date.now()
+        console.log(`demo took ${(endTime - startTime) / 1000} seconds.`)
+    })
 }
 
 let program_end = false // prevent node.js exit while async code runs
 
-ff(addPath).then(f => demo_f(f)).then(() => ff(addPath)).then(f => demo_f(f))
-.catch((oops) => console.log('oops ~ ' + oops))
-.then(() => {
-    let endTime = Date.now()
-    console.log(`program took ${(endTime - startTime) / 1000} seconds.`)
-    program_end = true
-});
+demo_f(addPath).then(() => demo_f(addPath))
+    .catch((oops) => console.log('oops ~ ' + oops))
+    .then(() => {
+        program_end = true
+    });
 
 // name ideas..
 // source branch > hunt, rope, lint, ffetch, use, aquire, get, ff, defn, foo, reqf, ask, arc, draw, canopy
