f7959692b6fda877e38c2e291aa23736e25ce88a
unknown <46693400+devmachiine@users.noreply.github.com>
2019-02-25 22:50:59 +0100

    One time test - remote code changes didn't propagate (see gist history)

--- tests/self-contained/remote/remote-load-remote.js
+++ tests/self-contained/remote/remote-load-remote.js
@@ -1,6 +1,6 @@
 (test, assert) => {
 
-    let test_remote_call_remote = ff('https://gist.githubusercontent.com/devmachiine/44c86b61bd8b98226d7ddbe97b4196ea/raw/1528e221d7b4eef684eb8424286cc2506d869081/remote-remote.js')
+    let test_remote_call_remote = ff('https://gist.githubusercontent.com/devmachiine/44c86b61bd8b98226d7ddbe97b4196ea/raw/66107ef784c8a72bb99fdac2acd23bdd4a8280f9/remote-remote.js')
 
     let test_add = await test_remote_call_remote(test, assert)
 
