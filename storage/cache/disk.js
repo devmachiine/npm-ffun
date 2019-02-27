15bf3b687b8872e7365669977c651af22a5ad3eb
unknown <46693400+devmachiine@users.noreply.github.com>
2019-02-27 11:39:27 +0100

    Change file cache path to same as remote url

--- storage/cache/disk.js
+++ storage/cache/disk.js
@@ -22,14 +22,22 @@ module.exports = function (cache_dir, fetch_code) {
         fs.readFile(file, (err, data) => resolve(err ? undefined : data))
     })
 
-    let disk_add = (filePath, value) => new Promise((resolve, reject) => {
-        fs.writeFile(filePath, value, function (err) {
-            if (err) reject(err)
-            else resolve(value)
-        });
+    let disk_add = (file_path, value) => new Promise((resolve, reject) => {
+        try {
+            let file_dir = path.dirname(file_path)
+            if (!fs.existsSync(file_dir)) {
+                fs.mkdirSync(file_dir, { recursive: true })
+            }
+            fs.writeFileSync(file_path, value)
+            resolve(value)
+        }
+        catch (err) {
+            print('err --> ' + err)
+            reject(`Error in ${path.basename(__filename)}: ${err}`)
+        }
     })
 
-    let valid_filename = (data) => data.replace(/\//g, '-').replace(/\:/g, '_')
+    let valid_filename = (data) => data.replace('https://', '') + '.txt'
 
     let file_path = (key) => {
         if (!key.startsWith(`https://`)) {
@@ -39,7 +47,7 @@ module.exports = function (cache_dir, fetch_code) {
             // -> for other forks/download of this repo to work correctly 
             return key
         }
-        else return `${cache_dir}/${valid_filename(key)}.txt`
+        else return path.join(cache_dir, valid_filename(key))
     }
     // todo folder(s) for files and .external for remote dependency
 
@@ -57,10 +65,9 @@ module.exports = function (cache_dir, fetch_code) {
             } else {
                 print('disk cache miss')
 
-                let namepart = path.basename(filename)
-                print('namepart:' + namepart)
-                if (!namepart.startsWith('https_--'))
-                    throw `Could not find file: ${filename}`
+                if (!filename.startsWith(cache_dir)) {
+                    throw `Error in ${path.basename(__filename)}: Invalid cache directory for file: ${filename}`
+                }
 
                 return fetch_code(dependency).then((val) => disk_add(filename, val))
             }
