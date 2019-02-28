57bba4519d7673eb16ee3aef5521ddf9838e8932
unknown <46693400+devmachiine@users.noreply.github.com>
2019-02-28 23:20:06 +0100

    Update test scope description

--- tests/external-setup/test-scope.js
+++ tests/external-setup/test-scope.js
@@ -1,6 +1,6 @@
 (framework) => {
     let { test, assert } = framework
-    return test('this function has access to test methods', () => {
+    return test('this function has simple access to outside scope', () => {
         assert(`${_outer_val} + 2 == 2`)
     })
 }
