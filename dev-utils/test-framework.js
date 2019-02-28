c0e6397cbdc08ceb9d58782d3a416186fe5a6941
unknown <46693400+devmachiine@users.noreply.github.com>
2019-02-28 23:47:00 +0100

    Assert (falsey, falsey) bugfix, Add async test, Add stacktrace

--- dev-utils/test-framework.js
+++ dev-utils/test-framework.js
@@ -1,5 +1,5 @@
 const assert = (assumption, expected) => {
-    if (expected) {
+    if (typeof expected !== 'undefined') {
         if (assumption !== expected)
             throw `Evaluation [${quote_wrap(assumption)}] === [${quote_wrap(expected)}]`
     }
@@ -9,14 +9,36 @@ const assert = (assumption, expected) => {
 
 const quote_wrap = (value) => typeof value === 'string' ? `'${value}'` : value
 
+const ok_result = (description) => {
+    return { description: description }
+}
+
+const error_result = (description, err) => {
+    return {
+        description: description,
+        error: '' + err,
+        error_stack: err.stack
+    }
+}
+
 const test = (description, func) => {
     try {
         func()
-        return { description: description }
+        return ok_result(description)
     } catch (err) {
-        return { description: description, error: err }
+        return error_result(description, err)
     }
 }
+
+const test_async = (description, promise, func) => promise
+    .then(result => result)
+    .catch(err => {
+        err.message = 'Promise rejected >> ' + err.message
+        throw err
+    })
+    .then((result) => func(result))
+    .then(_ => ok_result(description))
+    .catch(err => error_result(description, err))
 
 const display_message = test => {
     let prefix = test.error ? 'error' : 'ok'
@@ -24,11 +46,17 @@ const display_message = test => {
     return `[${prefix}] ${test.description}${postfix}`
 }
 
+const result_text = test_result => {
+    let short_message = display_message(test_result)
+    let postfix_stack = test_result.error_stack ? '\nstack:\n' + test_result.error_stack : ''
+    return `${short_message}${postfix_stack}`
+}
+
 const assert_test = (result, error_message) => {
     if (result.error != error_message) {
         let err = !error_message ? result.error :
             `test did not fail as expected. ${result.error ?
-                `(${error_message} <--> ${result.error})` : 'No errors.'}`
+                `[${error_message}] === [${result.error}]` : 'No errors.'}`
         throw `\n [x] ${result.description}\n      --> ${err}`
     }
 }
@@ -37,7 +65,7 @@ let tally_results = (...results) => {
     let ok_tests = 0, err_tests = 0, error_messages = ''
     results.forEach(result => {
         if (result.error) {
-            error_messages += display_message(result)
+            error_messages += result_text(result)
             err_tests++
         }
         else ok_tests++
@@ -47,5 +75,5 @@ let tally_results = (...results) => {
     return { overview, error_messages }
 }
 
-module.exports = { assert, test, display_message, tally_results, assert_test };
+module.exports = { assert, test, test_async, display_message, tally_results, assert_test };
 
