a1f5931df84bde4c59ebc2fc86996a43cb3b4d25
unknown <46693400+devmachiine@users.noreply.github.com>
2019-03-03 00:11:43 +0100

    Refactor, move todo list outside of app-test

--- app-test.js
+++ app-test.js
@@ -8,18 +8,18 @@
 
     let ff = require('./ffetch')()
     let test_framework = {
-        test, test_async, assert, display_message, tally_results
+        test, test_async, assert, tally_results
     } = require('./dev-utils/test-framework')
 
     /* Test basics */
 
-    let basic_tests = [
+    let basic_tests = await Promise.all([
 
         test_async("local root function ffetches and works",
             ff('./hello-fetch.js')('forest'),
             (result_hello) => assert(result_hello, 'Hello, forest!'))
 
-        , test_async("local simple arrow expected to fail (but works @ 2019.3.2)",
+        , test_async("local simple arrow expected to fail (but works @ 2019.3.3)",
             ff('./tests/external/arrow-only.ts1128')(),
             (result_hello) => assert(result_hello, 'not recommended to ommit variable or ()'))
 
@@ -45,11 +45,7 @@
         , test_async("non-url path expects to start with './'",
             test_async("ff without ./", ff('hello-fetch.js')('forest'), () => _na),
             (result) => assert(!!result.error, true))
-    ]
-
-    let completed_basic = await Promise.all(basic_tests)
-
-    print(tally_results('Basic', ...completed_basic))
+    ])
 
     /* Test behaviour from within a ffetched function */
 
@@ -60,35 +56,41 @@
             .filter(path => path.endsWith('.js'))
             .map(test_path => ff(test_path)(test, assert)))
 
+    /* Print tests results */
+
+    print(tally_results('Basic', ...basic_tests))
     print(tally_results('Extra', ...extra_tests))
 
-    // pending tests:
-    // [x] local load local
-    // [x] local load remote
-    // [x] remote load remote
-    // [x] remote load relative
-    // [x] pass ff func to ff func
-    // [ ] require available in ff (shouldn't it be? unless it's called _.njs?)
-
-    // Test  injection behavior
-    // [ ] create test regarding injection without explicit binding on function input (see test-scope.js)
-    // [ ] injection ~ ommit injected function is ok, but replacing it in unexpected order breaks.
-
-    // [ ] Scope insecure test -> prove tat access to outer still possible regardless of injection
-    // [x] change all throw to throw new Error to get stacktrace
-    // [ ] change all throw/catch to result type (if re-use simple)
-
-    // fforest.
-    // [ ] maybe - dependency upgrade, or signal ~ if it's to be part of this POC.
-
-    // tests
-    // [ ] root-relativity behavior
-
-    let end = new Date().getTime();
-    let time = end - start_time;
-    print(`Completed in ~ ${time} ms`)
+    let end_time = new Date().getTime()
+    let elapsed = end_time - start_time
+    print(`Completed in ~ ${elapsed} ms`)
     print('_'.repeat(65))
+
 })().catch(err => {
     console.log('🎣 🦖 app-test err!\n')
     console.log(err.stack ? err.stack : '' + err)
 })
+
+/*  pending tests:
+[x] local load local
+[x] local load remote
+[x] remote load remote
+[x] remote load relative
+[x] pass ff func to ff func
+[ ] require available in ff (shouldn't it be? unless it's called _.njs?)
+
+Test  injection behavior
+[ ] create test regarding injection without explicit binding on function input (see test-scope.js)
+[ ] injection ~ ommit injected function is ok, but replacing it in unexpected order breaks.
+
+[ ] Scope insecure test -> prove tat access to outer still possible regardless of injection
+[x] change all throw to throw new Error to get stacktrace
+[ ] change all throw/catch to result type (if re-use simple)
+
+fforest.
+[ ] maybe - dependency upgrade, or signal ~ if it's to be part of this POC.
+
+tests
+[ ] root-relativity behavior
+
+*/
