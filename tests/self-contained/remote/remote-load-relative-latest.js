834712cffe9d22b7660915f5ce80f0a21e0e5332
unknown <46693400+devmachiine@users.noreply.github.com>
2019-03-03 18:46:23 +0100

    Confirm stable dependency even when remote latest behavior changes.

--- tests/self-contained/remote/remote-load-relative-latest.js
+++ tests/self-contained/remote/remote-load-relative-latest.js
@@ -1,9 +1,16 @@
 () => {
     let unstable_latest_and_greatest = 'https://raw.githubusercontent.com/devmachiine/ffetch-test/master/demo-functions/math/square.js'
 
+    // note even though it points to master branch, cache version still behaved as expected.
+
+    // todo ~ create tool that deletes selected functions/folders from cache
+    // run tests to check behavior,
+    // and on failure reverts function-relevant commits backwards till tests pass.
+
     return test('remote load remote relative - latest and who knows what changed',
         ff(unstable_latest_and_greatest)(6),
         (six_squared) => {
-            assert(six_squared, 36)
+            let off_by_one_error = 1
+            assert(six_squared, 36 + off_by_one_error)
         })
 }
--- tests/self-contained/remote/remote-load-relative-stable.js
+++ tests/self-contained/remote/remote-load-relative-stable.js
@@ -1,6 +1,10 @@
 () => test('remote load remote relative - stable',
     (async () => {
+        
+        // remote repo @ be50423ffb17853bfeddc1268aff4dc582b50279
         let stable_at_point_in_time = 'https://raw.githubusercontent.com/devmachiine/ffetch-test/be50423ffb17853bfeddc1268aff4dc582b50279/demo-functions/math/square.js'
+        
         let twelve_squared = await ff(stable_at_point_in_time)(12)
+
         assert(twelve_squared, 144)
     }))
